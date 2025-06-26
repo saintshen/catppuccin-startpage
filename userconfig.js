653687e5a7cba1d1624e13dee832248b12ac95f7
Ming Shen <saintshen@gmail.com>
2025-06-25 22:59:00 -0400

    chores: adjust links

--- userconfig.js
+++ userconfig.js
@@ -138,6 +138,65 @@ const default_configuration = {
             },
           ],
         },
+        {
+          name: "general",
+          links: [
+            {
+              name: "alternative-to",
+              url: "https://alternativeto.net/",
+              icon: "box-multiple",
+              icon_color: palette.green,
+            },
+            {
+              name: "deepl",
+              url: "https://www.deepl.com",
+              icon: "language",
+              icon_color: palette.peach,
+            },
+            {
+              name: "monkeytype",
+              url: "https://monkeytype.com/",
+              icon: "keyboard",
+              icon_color: palette.red,
+            },
+            {
+              name: "dub",
+              url: "https://dub.co/",
+              icon: "external-link",
+              icon_color: palette.blue,
+            },
+            {
+              name: "speed-test",
+              url: "https://librespeed.org/",
+              icon: "brand-speedtest",
+              icon_color: palette.mauve,
+            },
+            {
+              name: ".to",
+              url: "https://rentry.co/tosites",
+              icon: "arrow-loop-right",
+              icon_color: palette.sky,
+            },
+            {
+              name: "cloudconvert",
+              url: "https://cloudconvert.com/",
+              icon: "transform",
+              icon_color: palette.lavender,
+            },
+            {
+              name: "is-it-down",
+              url: "https://downforeveryoneorjustme.com/",
+              icon: "server-off",
+              icon_color: palette.flamingo,
+            },
+            {
+              name: "selfh.st",
+              url: "https://selfh.st/",
+              icon: "server-2",
+              icon_color: palette.pink,
+            },
+          ],
+        },
       ],
     },
     {
@@ -584,65 +643,6 @@ const default_configuration = {
       name: "tools",
       background_url: "src/img/banners/banner_17.gif",
       categories: [
-        {
-          name: "general",
-          links: [
-            {
-              name: "alternative-to",
-              url: "https://alternativeto.net/",
-              icon: "box-multiple",
-              icon_color: palette.green,
-            },
-            {
-              name: "deepl",
-              url: "https://www.deepl.com",
-              icon: "language",
-              icon_color: palette.peach,
-            },
-            {
-              name: "monkeytype",
-              url: "https://monkeytype.com/",
-              icon: "keyboard",
-              icon_color: palette.red,
-            },
-            {
-              name: "dub",
-              url: "https://dub.co/",
-              icon: "external-link",
-              icon_color: palette.blue,
-            },
-            {
-              name: "speed-test",
-              url: "https://librespeed.org/",
-              icon: "brand-speedtest",
-              icon_color: palette.mauve,
-            },
-            {
-              name: ".to",
-              url: "https://rentry.co/tosites",
-              icon: "arrow-loop-right",
-              icon_color: palette.sky,
-            },
-            {
-              name: "cloudconvert",
-              url: "https://cloudconvert.com/",
-              icon: "transform",
-              icon_color: palette.lavender,
-            },
-            {
-              name: "is-it-down",
-              url: "https://downforeveryoneorjustme.com/",
-              icon: "server-off",
-              icon_color: palette.flamingo,
-            },
-            {
-              name: "selfh.st",
-              url: "https://selfh.st/",
-              icon: "server-2",
-              icon_color: palette.pink,
-            },
-          ],
-        },
         {
           name: "piracy",
           links: [
